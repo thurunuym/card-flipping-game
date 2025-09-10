fe4c448a567493e272f7ebed069537fbf8be988c
thurunuym <marasinghe.ty@gmail.com>
2025-09-11 00:21:49 +0530

    checkforMatch function

--- script.js
+++ script.js
@@ -48,5 +48,30 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+        function checkForMatch() {
+        const cards = document.querySelectorAll('#game-board img');
+        const firstCardId = cardsChosenId[0];
+        const secondCardId = cardsChosenId[1];
+
+        if (cardsChosen[0] === cardsChosen[1] && firstCardId !== secondCardId) {
+            cards[firstCardId].style.visibility = 'hidden';
+            cards[secondCardId].style.visibility = 'hidden';
+            cards[firstCardId].removeEventListener('click', flipCard);
+            cards[secondCardId].removeEventListener('click', flipCard);
+            cardsWon.push(cardsChosen);
+        } else {
+            cards[firstCardId].setAttribute('src', 'images/blank.png');
+            cards[secondCardId].setAttribute('src', 'images/blank.png');
+        }
+
+        cardsChosen = [];
+        cardsChosenId = [];
+
+        if (cardsWon.length === cardArray.length / 2) {
+            alert('Congratulations! You found them all!');
+        }
+    }
+
+    startButton.addEventListener('click', createBoard);
 
 });
